5a695ce2148e5558112c94a7ff3eec3bec1d7d63
Hanis <67252046+hanisdhuzuki@users.noreply.github.com>
2022-06-25 18:14:04 +0800

    Update SmartAppRoom_GroupE.asm

--- SmartAppRoom_GroupE.asm
+++ SmartAppRoom_GroupE.asm
@@ -1,67 +1,51 @@
 # GROUP E CAAL SEC 02
-# SMART APP ROOM
+# SMART ROOM APP
 # NURUL HANIS 1913364
 # AIN SHAHEADA 1920162
 
 .data
-#--------------------------------------------
-#selection screen
 
+#data for detection
+device:			.space 10  
+motion: 		.byte 1, 0
+
+#selection screen
 welcome:		.asciiz	"____________________________ \n Hello, User! \n Welcome to Smart Room app! \n____________________________ \n" 
 deviceask:		.asciiz	" \n\nSelect a device by entering a number.  \n\n\t 1. Air Conditioner \n\t 2. Light Bulb \n\t 3. Fire Alarm \n\n ENTER A NUMBER (1/2/3) : "
-connection:		.asciiz "Wifi connection? (Y/N): "
+connection:		.asciiz "\n\nWifi connection? (Y/N): "
 wrongMsg:		.asciiz "\n\n\t\<<----!!!!!!		INVALID		!!!!!!---->\n\t\<<----!!!!!!	ENTER THE INPUT AGAIN	!!!!!!---->"
-#--------------------------------------------
-#Light section
 
-motion: 		.byte 1, 0
+#Light section
 motiondetect:		.asciiz ". There is motion detected! \nThe light is ||ON||. "
 nomotiondetect:		.asciiz ". There is no motion detected! \nThe light is ||OFF||. "
-#--------------------------------------------
-# temperature/AC section
 
+# temperature/AC section
 getTempMsg1: 		.asciiz "Enter the current temperature (Celcius): "
 niceTempMsg: 		.asciiz "\nNice temperature! Have a great day.<3"
 getTempMsg2: 		.asciiz "\tRoom temperature is hot!\n\t!!!MORE THAN (or and equal to) 32 DEGREE CELCIUS!!!\n\t<<---AC ||ON|| --->>\n\tEnter your desired temperature (<32 degree Celcius):"
 
-#--------------------------------------------
-#Fire Alarm section
-
+#smoke
+getSmoke:		.asciiz "Smoke? (Y/N): "
 gasvalue: 		.byte 250
-alertON:	.asciiz "\n\t------------------------------------------\n\t\t\t WARNING!!!! \n\t\t GAS VALUE IS HIGH!!!!! \n\n \t\t<<--- BUZZER ||ON|| --->>\n\t\t<<--- LED ||ON|| ---> \n\t ------------------------------------------\n"
-alertOFF:	.asciiz "\n\t------------------------------------------\n\ \n\t\t GAS VALUE IS NORMAL \n\n \t\t<<--- BUZZER ||OFF|| --->>\n\t\t<<--- LED ||OFF|| ---> \n\t ------------------------------------------\n"
-askalarm:	.asciiz "\n\t------------------------------------------\n\ \n\t\t TURN OFF ALARM? \n\t\t  Enter number 1, for turn off alarm, enter number 2, to not turn off the alarm  \n\t ------------------------------------------\n"
-alarmOFF:	.asciiz "\n\t-----------------------------\n\ \n\t ALARM OFF  \n\t -----------------------------\n"
-noturnoffalarm: .asciiz "\n\t-----------------------------\n\ \n\t ALARM ON  \n\t -----------------------------\n"
-#--------------------------------------------
+
+alertON:		.asciiz "\n\t------------------------------------------\n\t\t\t WARNING!!!! \n\t\t GAS VALUE IS HIGH!!!!! \n\n \t\t<<--- BUZZER ||ON|| --->>\n\t\t<<--- LED ||ON|| ---> \n\t ------------------------------------------\n"
+alertOFF:		.asciiz "\n\t------------------------------------------\n\ \n\t\t GAS VALUE IS NORMAL \n\n \t\t<<--- BUZZER ||OFF|| --->>\n\t\t<<--- LED ||OFF|| ---> \n\t ------------------------------------------\n"
+askalarm:		.asciiz "\n\t------------------------------------------\n\ \n\t\t TURN OFF ALARM? \n\t\t  Enter number 1, for turn off alarm, enter number 2, to not turn off the alarm  \n\t ------------------------------------------\n"
+alarmOFF:		.asciiz "\n\t-----------------------------\n\ \n\t ALARM OFF  \n\t -----------------------------\n"
+noturnoffalarm: 	.asciiz "\n\t-----------------------------\n\ \n\t ALARM ON  \n\t -----------------------------\n"
 
 
 .text
 
-#--------------------------------------------
+#-------------------------------------------------------------
 # selection screen
-#--------------------------------------------
+#-------------------------------------------------------------
 
 # Print out  welcome message 
 	la 	$a0,welcome
 	jal	PrintString
 
-# Print out  to ask device
-selectDevice:
-	la 	$a0,deviceask 
-	jal	PrintString
-
-#Get input from User, to connect to the device.
-#syscall read integer
-	li 	$v0, 5
-	syscall
-
-# if integer >3
-	bgt	$s1,3,getDesiredTemp #(input is >3)
-	la	$a0,wrongMsg
-	jal	PrintString
-	j	selectD
-
+wifi:
 # Print out  "Wifi connection? (Y/N)" 
 	la 	$a0,connection 
 	jal	PrintString
@@ -70,32 +54,37 @@ selectDevice:
 	syscall #read char
 	
 	addi	$t1,$v0,0
-	#beq	$t1,'Y',Selection #if user enters Y, then go to Selection
-	#beq	$t1,'y',Selection #if user enters y, then go to Selection
-	beq	$t1,'N',selectDevice #if user enters N, then go to selectDevice
- 	beq	$t1,'n',selectDevice #if user enters n, then go to selectDevice
+	beq	$t1,'Y',selectDevice #if user enters Y, then go to Selection
+	beq	$t1,'y',selectDevice #if user enters y, then go to Selection
+	beq	$t1,'N',wifi #if user enters N, then go to selectDevice
+ 	beq	$t1,'n',wifi #if user enters n, then go to selectDevice
  	la	$a0,wrongMsg
 	jal	PrintString
 	j	selectDevice	
 
 #device selection by the user
-#Selection:
-	#lb	$s1,selectedDevice($t2) #to print integer from the user
-	#li	$v0,1	
-							
-	#addi	$a0,$s1,0
-	#syscall #to print integer
+selectDevice:
+	la 	$a0,deviceask 
+	jal	PrintString
+
+selectedDevice:
+	#Get input from User,to enter a number from 1 to 3
+	#syscall read integer
+	li 	$v0, 5
+	syscall
+	addi	$s1,$v0,0  #s1 has the user input
+
+	move	$t4, $v0# move user input into t4
+	addi 	$t5, $zero, 1
+	addi	$t6, $zero, 2
+	addi	$t7, $zero, 3		# add each of these to use for comparison below
+	beq	$t4, $t5, TemperatureDetector
+	beq	$t4, $t6, light
+	beq	$t4, $t7, firealarm
 	
-	#li	$v0,4				
-	#beq	$t3,1,TemperatureDetector
-	#beq	$t3,2,light
-	#beq	$t3,3,smokeDetector
-	#jal	PrintString
-
-#--------------------------------------------
+#-------------------------------------------------------------
 # temperature/AC section
-#--------------------------------------------
-
+#-------------------------------------------------------------
 TemperatureDetector:
 	# Print out "Enter the current temperature (Celcius): "
 	la	$a0, getTempMsg1
@@ -139,11 +128,9 @@ inputNiceTemp:
 	la	$a0,niceTempMsg
 	jal	PrintString
 
-
-
-#--------------------------------------------
-# Light section
-#--------------------------------------------
+#-------------------------------------------------------------
+# Light/motion detection section
+#-------------------------------------------------------------
 light:
 
 #Input for motion
@@ -169,11 +156,9 @@ detectnomotion:
 	la	$a0,motiondetect
 	j	End
 
-
-#--------------------------------------------
-# Smoke detector section
-#--------------------------------------------
-
+#-------------------------------------------------------------
+# Fire alarm/Smoke detector section
+#-------------------------------------------------------------
 firealarm:
 
 #Input for gas value
@@ -189,19 +174,14 @@ smokein:
 	bge	$s1,300,highgasvalue 	#If gas value higher or equal 300 
  	blt 	$s1,300,lowgasvalue	#If gas value lower than 300
 		
-
 #Print action message when gas value is high
-highgasvalue:	
-							
+highgasvalue:							
 	la	$a0,alertON
 	j	askturnoffalarm
-	
-	
+		
 #Ask user if they want to turn off the alarm 
 askturnoffalarm:	
 	la	$a0,askalarm
-	
-	
 	
 yesoffalarm:	
 	#syscall read integer
@@ -215,21 +195,18 @@ yesoffalarm:
 turnoffalarm:	
 	la	$a0,alarmOFF
 	
-	
 nooffalarm:
 	beq	$s1,2,noturnoffalarm
 	j	End
 	
 #Print action message when gas value is low	
-lowgasvalue:	
-							
+lowgasvalue:							
 	la	$a0,alertOFF
 	j	End
-	
 
 #--------------------------------------------
-	
-	
+
+#FUNCTIONS		
 #end the program
 End:
 	li $v0, 10
